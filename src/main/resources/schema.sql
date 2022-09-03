69305bf4585962a4568c13b71db3f27f74be587e
tochmary <tochmary@yandex-team.ru>
2022-09-03 12:08:16 +0300

    sprint16: 5. Docker Compose и продвинутая работа с контейнерами

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,4 +1,4 @@
-DROP TABLE IF EXISTS tags, items, users;
+--DROP TABLE IF EXISTS tags, items, users;
 
 --Таблица пользователей
 CREATE TABLE IF NOT EXISTS users ( id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY, email varchar(320), first_name varchar(100), last_name varchar(100), registration_date timestamp, state varchar(50) );
